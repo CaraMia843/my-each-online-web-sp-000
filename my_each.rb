eef95bc2f9266c619a49747ea5c6786898b273b7
Cara Zellner <carajeanine@gmail.com>
2020-10-11 17:18:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,10 @@
 def my_each(array)
-  if block_given?
+
     counter = 0 
      while counter < array.length
       yield array[counter]
       counter = counter += 1 
      end
-  else
-    "No block was given."
-  end
+ array
+  
 end
